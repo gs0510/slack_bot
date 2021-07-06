38eb17677fa4c13abe8235a18c29c5c77423b140
Gargi Sharma <gs051095@gmail.com>
2021-07-06 10:13:41 +0200

    This week's skips

--- src/curl_requests.ml
+++ src/curl_requests.ml
@@ -26,41 +26,44 @@ let get_members channel =
          id <> "UNZD1GY4W" (*lyrm*) &&
            id <>  "U0PFW68A3" (*engil*) &&
            id <> "UEDTALRKR" (*Celine*) &&
-          id <> "U013SFKC15M" (* Antonin Décimo *) &&
-         id <> "U01FFLZG0TZ" (*ngoguey*) 
     		 id <> "U0218CTLAF2" (*Umashankar*) &&
-          id <> "U0U6CJGH0" (*dinosaure*) &&
-         id <> "UDRKCMFCP" (*craigfe*) &&
-         id <> "UHG9PG222" (*NathanReb*) &&
+         id <> "UQ7RKM5U7" (*patrick*) &&
+         id <> "U022P9TQ76X" (* odinaka joy*) &&
+         id <> "U0JP4EH7H" (*samoht*) &&
+         id <> "UFJNZ2ZH9"(*Jules*) &&
          *)
          (*folks who skip this week*)
-         && id <> "UNQPQU9UH"
-         (*gargi*) && id <> "U016FMK46NR"
-         (*Ulugbek*) && id <> "UQ7RKM5U7"
-         (*patrick*) && id <> "U9GE7FGTH"
-         (* lortex *) && id <> "U023HS3GFPX"
-         (*Christine Rose*) && id <> "ULYMRQKAL"
-         (*iona*) && id <> "U0XKUH6LB"
-         (*trefis*) && id <> "U0J5T0YUD"
-         (*gemma-gordon*) && id <> "U0JP4EH7H"
-         (*samoht*) && id <> "U0J5U03J4"
-         (*avsm*) && id <> "UFJNZ2ZH9"
-         (*Jules*) && id <> "U01M5NDAD8Q"
-         (* Gabriel Belouze *) && id <> "U0JMF1GRW"
-         (*def*) && id <> "U0JCSR1HT"
-         (* magnus *) && id <> "USAEFBTSS"
-         (*ulysse*) && id <> "UAP0GA934"
-         (* zshipko *) && id <> "UEQMNGNH0"
-         (*pascutto*) && id <> "U023CTFM92L"
-         (* dikshagupta*) && id <> "U023CTF6A56"
-         (*shreyaswikriti*) && id <> "U022P9TQ76X"
-         (* odinaka joy*) && id <> "U845EHFPT"
-         (*Kate*) && id <> "U0J6HJZ29"
-         (* kc*)
+
+         id <> "UDRKCMFCP" (*craigfe*) &&
+         id <> "UNQPQU9UH" (*gargi*) &&
+         id <> "U0J5T0YUD" (*gemma-gordon*) &&
+         id <> "U023HS3GFPX" (*Christine Rose*) &&
+         id <> "USAEFBTSS" (*ulysse*) &&
+         id <> "UHG9PG222" (*NathanReb*) &&
+         id <> "U01FFLZG0TZ" (*ngoguey*) 
+         id <> "U023CTF6A56" (*shreyaswikriti*) &&
+         id <> "U01M5NDAD8Q" (* Gabriel Belouze *) &&
+         id <> "ULYMRQKAL" (*iona*) &&
+         id <> "U023CTFM92L" (* dikshagupta*) &&
+         id <> "U0XKUH6LB" (*trefis*) &&
+         id <> "U845EHFPT" (*Kate*) &&
+         id <> "U0JMF1GRW" (*def*) &&
+          id <> "U0U6CJGH0" (*dinosaure*) &&
+         id <> "U016FMK46NR" (*Ulugbek*) &&
+         id <> "UAP0GA934" (* zshipko *) &&
+         (* people on vacation*)
+         id <> "UEQMNGNH0" (*pascutto*) &&
+         id <> "U0JCSR1HT" (* magnus *) &&
+
+         (* folks skipping until September*)
+        id <> "U0J6HJZ29" (* kc*) &&
+        id <> "U0J5U03J4" (*avsm*) &&
+        id <> "U9GE7FGTH" (* lortex *) &&
+        id <> "U013SFKC15M" (* Antonin Décimo *) &&
+
          (*folks who skip permanently*)
-         && id <> "U0118JHAUG7"
-         (* yman *) && id <> "UU5DVAJQ6"
-         (* Romain Liautaud *))
+        id <> "U0118JHAUG7"(* yman *) &&
+        id <> "UU5DVAJQ6" (* Romain Liautaud *))
 
 let write_to_slack channel output =
   let args_message =

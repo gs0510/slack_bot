0ba91d55f7bc4375106de5d898c79901a831de5f
Gargi Sharma <gs051095@gmail.com>
2021-11-12 17:05:48 +0100

    remove scripts

--- weekly_opt_out.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-test_channel=C013R8HP3P1
-real_channel=C011F85NCNT
-
-HTTP_CODE=$(curl https://slack.com/api/chat.postMessage -F token=xoxb-18193414053-1098751527969-2WaxTFvOO9yj4bf9JpGTJ1yD -F channel=${test_channel} -F text="Who wants to skips this week <!here>? React with a :raised_hand::skin-tone-4:")
-echo $HTTP_CODE
